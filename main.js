660cf9b217e01f2149aaa5af89a24742a30bc2fb
nautimon <nautisignup@gmail.com>
2026-06-11 00:20:59 +0000

    Print coordinates

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 // Write your code below
-
+// Write your code below
+function printCoordinates(size) {
+    for (var x = 0; x < size; x++) {
+        for (var y = 0; y < size; y++) {
+            console.log("(" + x + ", " + y + ")");
+        }
+    }
+}
